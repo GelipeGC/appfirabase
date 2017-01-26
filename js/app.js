c45a80fccda59a311847a01cef163d939ebe36dd
Felipe Guzman <felipe-guzman.c@hotmail.com>
2017-01-26 16:11:23 -0600

    Crud basico de usuarios capturando errores y evitando registros repetidos

--- js/app.js
+++ js/app.js
@@ -18,16 +18,57 @@ var config = {
 			for (player in snapshot.val()) {
 				console.log(player, ',', data[player]);
 				row += "<tr>" +
-							"<td>" + player + "</td>" +
-							"<td>" + data[player].mail + "</td>" +
-							"<td>" + data[player].number + "</td>" +
-							"<td>" + data[player].position + "</td>" +
-
+							"<td class=\"playerName\">" + player + "</td>" +
+							"<td class=\"mail\">" + data[player].mail + "</td>" +
+                			"<td class=\"number\">" + data[player].number + "</td>" +
+                			"<td class=\"position\">" + data[player].position + "</td>" +
+							"<td> <div class=\"btnEdit btn btn-warning glyphicon glyphicon-edit\"></div> </td>" +
+							"<td> <div class=\"btnDelete btn btn-danger glyphicon glyphicon-remove\"></div></td>" +
 						"</tr>"
 			}
 
 			$("#playersTable tbody").append(row);
 			row = "";
+
+			//*** Delete record from firebase
+			$(".btnDelete").click(function(){
+				console.log('clicked')
+				var selectedPlayer = $(this).closest("tr")
+				.find(".playerName")
+				.text();
+
+				console.log(selectedPlayer)
+				rootRef.child(selectedPlayer).remove()
+			})
+
+			//*** edit record from firebase
+			$(".btnEdit").click(function(){
+				console.log('clicked')
+				var selectedPlayer = $(this).closest("tr")
+					.find(".playerName")
+					.text();
+
+				//asign ddata to from fields
+				$("#fullName").val($(this).closest("tr").find(".playerName").text());
+				$("#mail").val($(this).closest("tr").find(".mail").text());
+				$("#number").val($(this).closest("tr").find(".number").text());
+				$("#position").val($(this).closest("tr").find(".position").text());
+            	$("#btnSend").text("Actualizar").removeClass("btn-primary").addClass("btn-warning").unbind("click").click(function() {
+
+            		rootRef.child(selectedPlayer).update({
+            			mail: $("#mail").val(),
+            			number: $("#number").val(),
+            			position: $("#position option:selected").text()
+            		}, function(){
+            			$("#fullName").val("");
+            			$("#mail").val("");
+            			$("#number").val("");
+            			$("position").val("");
+            			$("#btnSend").text("Enviar").removeClass("btn-warning").addClass("btn-primary").unbind("click").click(sendData);
+            		})
+				});
+            })
+
 		}, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
@@ -42,8 +83,21 @@ var config = {
             number: $("#number").val(),
             position: $("#position option:selected").text()
         }
+        var onComplete = function(error){
+        	if (error) {
+        		console.log(error, 'La sincronizacion fallo');
+        	} else {
+        		console.log(error, 'la sincronizacion ha sido exitosa');
+        	}
+        }
 
-        rootRef.child(fullName).set(dataPlayer)
+        rootRef.once('value', function(snapshot){
+        	if (snapshot.hasChild(fullName)) {
+        		$('#myModal').modal('show');
+        	} else {
+        		rootRef.child(fullName).set(dataPlayer, onComplete);
+        	}
+        })
 
     })
 });
